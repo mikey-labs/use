97e8989b1a5eea27c446231b00272c95f50f5eff
Mikey <zxiangyun@sph.com.sg>
2023-02-17 21:27:07 +0800

    add event listener

--- packages/lib-use/src/useClickOutSide/index.ts
+++ packages/lib-use/src/useClickOutSide/index.ts
@@ -1,11 +1,11 @@
-import { inBrowser } from "../utils";
+import {inBrowser} from "../utils";
 
 type UseClickOutSideOptions = {
     eventName:string
 }
 
 export const useClickOutSide = (
-    target:Element,
+    target:any,
     listener: EventListener,
     options:UseClickOutSideOptions = {
         eventName:'click'
@@ -16,9 +16,25 @@ export const useClickOutSide = (
     }
     const { eventName } = options;
     const eventListener = (event:Event)=>{
-        if(target && !target.contains(event.target as Node)){
+        if(target && !target.contains(event.target as HTMLElement)){
             listener(event);
         }
     }
+    const observer:MutationObserver = new MutationObserver((records:MutationRecord[])=>{
+        const isRemoved:boolean = records.some(
+            (record)=>Array.from(record.removedNodes).some(node=>node === target)
+        )
+        if(isRemoved){
+            document.removeEventListener(eventName,eventListener)
+            observer.disconnect();
+        }
+    });
+    observer.observe(
+        target.parentNode,
+        {
+            childList:true
+        }
+    )
     document.addEventListener(eventName,eventListener);
+
 }
--- packages/lib-use/src/utils/index.ts
+++ packages/lib-use/src/utils/index.ts
@@ -1,4 +1,1 @@
-export const getDataType = (o : any) : string => {
-    return Object.prototype.toString.call(o).match(/\[object\s(\w+.*?)\]/)[1];
-}
 export const inBrowser =  typeof window !== 'undefined' && 'onload' in window
